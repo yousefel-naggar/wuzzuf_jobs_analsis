e5a675331891f96ea4f56b0668fbfc11132297b0
yousef <114927261+yousefel-naggar@users.noreply.github.com>
2024-12-04 02:30:03 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,2 @@
 wuzzuf_base_url = 'https://wuzzuf.net/search/jobs'
 chrome_driver = r'chromedriver-win64/chromedriver.exe'
-mongo_username='yousefelnaggar'
-mongo_password='yousefelnaggar'
-
-def get_data():
-    return mongo_username , mongo_password
-
-
-
-print(mongo_username)
